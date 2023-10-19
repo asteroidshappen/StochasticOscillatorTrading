c5038233d312b8f6c1abc0ac4e272d90c567db2a
Pascal U. Förster <pascal.foerster.19@ucl.ac.uk>
2023-10-19 20:13:22 +0100

    Add crossing criterion, start using pandas rolling windows

--- main.py
+++ main.py
@@ -7,29 +7,109 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def min_Ndays(rates, Ndays=14):
-    minNdays = np.min(rates[-Ndays:]) if len(rates) > Ndays else np.min(rates)
-    return minNdays
+    return rates.rolling(window=Ndays).min()
 
 def max_Ndays(rates, Ndays=14):
-    maxNdays = np.max(rates[-Ndays:]) if len(rates) > Ndays else np.max(rates)
-    return maxNdays
+    return rates.rolling(window=Ndays).max()
 
-def current_market_rate(recentC, min14, max14):
-    return 100 * (recentC - min14) / (max14 - min14)
+def current_market_rate(recentClosing, min14, max14):
+    return 100 * (recentClosing - min14) / (max14 - min14)
 
 def moving_average_market_rate(rates, n_avg=3):
-    avg = np.average(rates[-n_avg:]) if len(rates) > n_avg else np.average(rates)
-    return avg
+    return rates.rolling(window=n_avg).mean()
+
+# def larger_than_previous(rates):
+#     if rates.loc[-1] > rates.loc[-2]:
+#         larger = True
+#     else:
+#         larger = False
+#     return larger
+
+def changing_sign(rates):
+    """
+    Determine whether there is a sign change from the second to last to the last element
+
+    :param rates: numerical values in a numpy array dataframe
+    :return: 1 for positive to negative sign change, -1 for negative to positive sign change, 0 for no sign change
+    """
+    if (rates[-1] > 0) and (rates[-2] < 0):
+        change = 1
+    elif (rates[-1] < 0) and (rates[-2] > 0):
+        change = -1
+    else:
+        change = 0
+    return change
+
+def crossing_down_or_up(rates_1, rates_2):
+    """
+    Determine where rate_1 crosses down or up across rate_2, and where they do not cross at all
+
+    :param rates_1: numerical values in a pandas dataframe that will do the crossing
+    :param rates_2: numerical values in a pandas dataframe that acts as the reference to be crossed
+    :return: 1 for rate_1 crossing down across rate_2, -1 for rate_1 crossing up across rate_2, 0 for no crossing
+    """
+    # rates_comb = pd.concat([rates_1, rates_2], axis=1)
+    rates_diff = rates_1 - rates_2
+    rates_diff_sign_changes = rates_diff.rolling(window=2).apply(changing_sign, raw=True)
+
+    return rates_diff_sign_changes
 
 
 
-def plot_market_rate(ticker, period):
+def oscillator_buy_sell(ticker, period):
     history = ticker.history(period=period)
     name = ticker.info["shortName"]
     shorthand = ticker.info["symbol"]
 
-    for item in ticker.info.items():
-        print(item)
+    currency = ticker.info["currency"]
+
+    rates = history["Close"]
+    dates = rates.index
+
+    min_recent = min_Ndays(rates)
+    max_recent = max_Ndays(rates)
+
+    current_market_rates = current_market_rate(rates, min_recent, max_recent)
+    moving_avg_current_market_rates = moving_average_market_rate(current_market_rates)
+
+    crossing_market_rates = crossing_down_or_up(current_market_rates, moving_avg_current_market_rates)
+
+
+
+    mpl.use('MacOSX')
+    fig, ax = plt.subplots(
+        2, 1,
+        figsize=(9, 4),
+        height_ratios=(5, 3),
+        layout='constrained',
+        sharex=True,
+    )
+
+
+    ax[0].plot(dates, moving_avg_current_market_rates, color='xkcd:light orange')
+    ax[0].plot(dates, current_market_rates, color='xkcd:orange')
+
+    ax[1].plot(dates, crossing_market_rates, color='xkcd:orange')
+
+
+    fig.suptitle(f"{name} ({shorthand})")
+    ax[0].set_ylabel(f"Market rate ({currency})")
+
+    ax[-1].set_xlabel("Date")
+
+    fig.savefig(f"plots/marketrates_{shorthand}.png", dpi=300)
+
+    return fig, ax
+
+
+
+def plot_market_price(ticker, period):
+    history = ticker.history(period=period)
+    name = ticker.info["shortName"]
+    shorthand = ticker.info["symbol"]
+
+    # for item in ticker.info.items():
+    #     print(item)
 
     currency = ticker.info["currency"]
 
@@ -73,15 +153,17 @@ def plot_market_rate(ticker, period):
     # cosmetics
     fig.suptitle(f"{name} ({shorthand})")
 
-    ax[0].legend()
+    ax[0].legend(reverse=True)
 
-    ax[0].set_ylabel(f"Rates ({currency})")
+    ax[0].set_ylabel(f"Price ({currency})")
     ax[1].set_ylabel(f"Daily growth ({currency})")
     ax[2].set_ylabel(f"Net change ({currency})")
 
     ax[-1].set_xlabel("Date")
 
-    fig.savefig("plots/marketrates.png", dpi=300)
+    fig.savefig(f"plots/marketprices_{shorthand}.png", dpi=300)
+
+    return fig, ax
 
 
 
@@ -92,4 +174,12 @@ if __name__ == "__main__":
     get_META_info = yf.Ticker("META")
     period = '6mo'
 
-    plot_market_rate(get_META_info, period)
+    fig, ax = plot_market_price(
+        get_META_info,
+        period
+    )
+
+    oscillator_buy_sell(
+        get_META_info,
+        period
+    )
